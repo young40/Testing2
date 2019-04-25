e019c8190c9cf67d71028f294ff53552899127f5
Peter Young <young40@gmail.com>
2019-04-26 01:03:58 +0800

    udpate

--- src/app/views/MainScene.lua
+++ src/app/views/MainScene.lua
@@ -2,10 +2,21 @@
 local MainScene = class("MainScene", cc.load("mvc").ViewBase)
 
 local function bind(node, nodeFunc, t, key)
+    local mt = getmetatable(t) or {}
+    dump(mt)
+    dump(node, nodeFunc)
+
+    local b = mt.b or {}
+    mt.b = b
+
+    local kb = b[key] or {}
+    b[key] = kb
+
+    kb[#kb + 1] = {node, nodeFunc}
     -- dump(node)
     -- dump(nodeFunc)
     -- dump(t)
-    dump(key)
+    -- dump(key)
 end
 
 local Bindable = {}
@@ -36,11 +47,37 @@ function Bindable:ctor(child)
         mt[key] = value
 
         print("update: " .. key .. "  " .. value)
+
+
+        local b = mt.b or {}
+        local kb = b[key]
+
+        dump(kb)
+        if kb then
+            for _, bojtt in pairs(kb) do
+                local obj, func = unpack(bojtt)
+                dump(obj)
+                dump(func)
+                func(obj, value)
+            end
+        end
     end
 
     setmetatable(child, mt)
 
     dump(mt)
+end
+
+function Bindable:bind(key, obj, func)
+    local mt = getmetatable(self) or {}
+
+    local b = mt[b] or {}
+    mt[b] = b
+
+    local kb = b[key] or {}
+    b[key] = kb
+
+    kb[#kb + 1] = {obj = func}
 end
 
 local DataSource = class("DataSource", Bindable)
@@ -74,7 +111,10 @@ function MainScene:onCreate()
     t.str = "大笑三声"
 
     bind(h1, h1.setString, t, "str")
+    print("bbbbbbbb")
     bind(h2, h2.setString, t, "str")
+
+    t.str = "爱你永不变️"
 
     -- setmetatable(t, {
     --                  __index = function(t, key)
