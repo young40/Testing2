4f1b272c0a9afcd6497b43ff04696e9d56ae85f9
Peter Young <young40@gmail.com>
2019-05-08 00:29:50 +0800

    update

--- src/packages/mvvm/BaseView.lua
+++ src/packages/mvvm/BaseView.lua
@@ -1,3 +1,11 @@
 local BaseView = class("BaseView", cc.Node)
 
+function BaseView:ctor()
+    if self.onCreate then
+        assert(type(self.onCreate) == "function", "onCreate should be function")
+
+        self:onCreate()
+    end
+end
+
 return BaseView
